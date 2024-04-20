c803d737b10003aac45672d6582ae805aace795b
latifbima <latiefbima18@gmail.com>
2024-04-20 21:15:21 +0700

    pertemuan 8 praktikum 1

---
+++ pertemuan 8/upload.php
@@ -0,0 +1,21 @@
+<?php
+    if (isset($_POST["submit"])) {
+        $targetDirectory = "documents/";
+        $targetFile = $targetDirectory . basename($_FILES["documentToUpload"]["name"]);
+        $documentfileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+
+        $allowedExtensions = array ("txt", "pdf", "doc", "docx");
+
+        $maxFileSize = 10 * 1024 * 1024;
+
+        if (in_array($documentfileType, $allowedExtensions) && $_FILES["documentToUpload"]["size"] <= $maxFileSize) {
+            if (move_uploaded_file($_FILES["documentToUpload"]["tmp_name"], $targetFile)) {
+                echo "Dokumen berhasil diunggah";                
+            } else {
+                echo "Gagal mengunggah dokumen";
+            }
+        }  else {
+            echo "Jenis dokumen tidak valid atau melebihi ukuran maksimum yang diizinkan";
+        }
+    }
+?>
